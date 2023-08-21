82e67ceb0d5ad929025b13ebbf0fc62d3552c3c3
Julia Lipniewicz <julia.modex21@gmail.com>
2023-08-21 16:44:30 +0200

    Create kata#38-complementaryDNA.jsx

---
+++ src/kata#38-complementaryDNA.jsx
@@ -0,0 +1,27 @@
+
+/// KATA #38 ///
+
+/* DESCRIPTION:
+Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+
+If you want to know more: http://en.wikipedia.org/wiki/DNA
+
+In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+
+Example: (input --> output) */
+
+
+function DNAStrand(dna){
+  
+  const DNAComplement = {
+    'G' : 'C',
+    'C' : 'G', 
+    'T' : 'A',
+    'A' : 'T'
+  }
+  
+  return dna.split('').map(elem => DNAComplement[elem]).join('')
+
+}
