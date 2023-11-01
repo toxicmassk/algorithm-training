76479e51c31098c720afa9056008151806196eda
Julia Lipniewicz <julia.modex21@gmail.com>
2023-11-01 11:41:34 +0100

    Kata #52

---
+++ src/kata#52-findTheAverage.jsx
@@ -0,0 +1,11 @@
+/// KATA #52 ///
+
+/* Description:
+
+Write a function which calculates the average of the numbers in a given list.
+
+Note: Empty arrays should return 0. */
+
+var findAverage= (array) => {  
+  return array.length === 0 ? 0 : array.reduce((acc, ind)=> acc + ind, 0)/array.length
+}
