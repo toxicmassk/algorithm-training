79191063a0532663599604b36ea5f5f74014fb4a
Julia Lipniewicz <julia.modex21@gmail.com>
2023-11-06 18:23:46 +0100

    Kata#53-getCharacterFromASCIIValue

---
+++ src/kata#53-getCharacterFromASCIIValue.jsx
@@ -0,0 +1,16 @@
+/// KATA#53 ///
+
+/* Description:
+
+Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+Example:
+
+65 --> 'A'
+97 --> 'a'
+48 --> '0 */
+
+
+function getChar(c){
+   return String.fromCharCode(c)
+}
