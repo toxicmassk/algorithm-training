60bcfd83549faad746750702ae3e70db403b4064
Julia Lipniewicz <julia.modex21@gmail.com>
2023-11-09 11:31:48 +0100

    Kata#55-xorLogicalOperator

---
+++ src/kata#55-exclusive"or"(xor)LogicalOperator.jsx
@@ -0,0 +1,13 @@
+/// KATA #55 ///
+
+/* Description:
+
+Task
+Since we cannot define keywords in Javascript (well, at least I don't know how to do it), your task is to define a function xor(a, b) where a and b are the two expressions to be evaluated. Your xor function should have the behaviour described above, returning true if exactly one of the two expressions evaluate to true, false otherwise. */
+
+function xor(a, b) {
+    if ((a == true && b == false) || (a == false && b == true))
+    return true
+    else
+    return false
+  }
