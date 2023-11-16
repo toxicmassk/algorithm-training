0dd7f9f23fc444159d92a70d197e5c27424a6159
Julia Lipniewicz <julia.modex21@gmail.com>
2023-11-16 19:29:45 +0100

    Kata#7-maximumSubarraySum

---
+++ src/kata#57-maximumSubArraySum.jsx
@@ -0,0 +1,20 @@
+/// KATA #57 ///
+
+/* Description: 
+
+The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or list of integers:
+
+maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])
+// should be 6: [4, -1, 2, 1]
+Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of the whole array. If the list is made up of only negative numbers, return 0 instead.
+
+Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
+*/
+
+var maxSequence = function(arr){
+    var currentSum = 0;
+    return arr.reduce(function(maxSum, number){
+        currentSum = Math.max(currentSum+number, 0);
+        return Math.max(currentSum, maxSum);
+    }, 0);
+}
