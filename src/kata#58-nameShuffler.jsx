f9bb2c24735134bb5da60f1fea20ff2c2008e4a9
Julia Lipniewicz <julia.modex21@gmail.com>
2023-11-17 10:43:21 +0100

    Kata#58 nameShuffler

---
+++ src/kata#58-nameShuffler.jsx
@@ -0,0 +1,12 @@
+/// KATA #58 ///
+
+/* Description: 
+Write a function that returns a string in which firstname is swapped with last name.
+
+Example(Input --> Output)
+
+"john McClane" --> "McClane john" */
+
+function nameShuffler(str){
+    return str.split(' ').reverse().join(' ');
+  }
