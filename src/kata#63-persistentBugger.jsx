284b7eeaf740a844aa04192de1ba15ff45f8ce2c
Julia Lipniewicz <julia.modex21@gmail.com>
2023-12-13 14:20:48 +0100

    Kata#63-persistentBugger

---
+++ src/kata#63-persistentBugger.jsx
@@ -0,0 +1,16 @@
+/// KATA #63 ///
+
+/* Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+For example (Input --> Output):
+
+39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+4 --> 0 (because 4 is already a one-digit number) */
+
+function persistence(num) {
+    if (num < 10) {
+        return 0;
+    }
+    return 1 + persistence([...num.toString()].reduce((acc, digit) => acc * parseInt(digit), 1));
+}
