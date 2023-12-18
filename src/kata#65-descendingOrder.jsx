a64cbf7f45b27bc5ece78e24525d72776acbe0f1
Julia Lipniewicz <julia.modex21@gmail.com>
2023-12-18 13:06:49 +0100

    Kata#65-descendingOrder

---
+++ src/kata#65-descendingOrder.jsx
@@ -0,0 +1,16 @@
+/// KATA #65 ///
+
+/* Description: 
+Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+Examples:
+Input: 42145 Output: 54421
+
+Input: 145263 Output: 654321
+
+Input: 123456789 Output: 987654321*/
+
+const descendingOrder = n => (
+    Number([...String(n)].sort(function(a, b){return b-a}).join('')) 
+    )
+    
