dffed0e77a07673ad592cf113299fb523c59fdaf
Julia Lipniewicz <julia.modex21@gmail.com>
2023-12-30 11:48:40 +0100

    kata#67-sumNumbersDigits

---
+++ src/kata#67-summingANumbersDigits.jsx
@@ -0,0 +1,15 @@
+/// KATA #67 ///
+
+/* Description:
+Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits.
+
+For example: (Input --> Output)
+
+10 --> 1
+99 --> 18
+-32 --> 5
+Let's assume that all numbers in the input will be integer values. */
+
+function sumDigits(number) {
+    return Math.abs(number).toString().split('').reduce((sum, i) => sum + +i, 0);
+    };
