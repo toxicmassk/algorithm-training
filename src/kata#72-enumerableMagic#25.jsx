c3e93598c452be056011edb8e1c2a7b4749fff03
Julia Lipniewicz <julia.modex21@gmail.com>
2024-01-18 12:27:28 +0100

    kata#72-enumerableMagic#25

---
+++ src/kata#72-enumerableMagic#25.jsx
@@ -0,0 +1,10 @@
+/// KATA #72 ///
+
+/* Description:
+
+Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+*/
+
+function take(arr, n) {
+    return arr.slice(0, n);
+  }
