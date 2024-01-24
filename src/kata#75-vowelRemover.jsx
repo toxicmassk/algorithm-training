57388f47170a06f90d71a062ff688ed4c9cff167
Julia Lipniewicz <julia.modex21@gmail.com>
2024-01-24 19:25:14 +0100

    kata#75-vowelRemover

---
+++ src/kata#75-vowelRemover.jsx
@@ -0,0 +1,17 @@
+/// KATA #75 ///
+
+/*
+Descirption:
+
+Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+
+Examples
+"hello"     -->  "hll"
+"codewars"  -->  "cdwrs"
+"goodbye"   -->  "gdby"
+"HELLO"     -->  "HELLO"*/
+
+function shortcut (string) {
+  return string.replace(/[aeiou]/g, '');
+}
+
