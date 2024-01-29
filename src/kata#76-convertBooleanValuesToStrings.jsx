c91ee196999b63415810dc26c4feb492060b79e7
Julia Lipniewicz <julia.modex21@gmail.com>
2024-01-29 12:24:38 +0000

    kata#76-convertBooleanValueToString

---
+++ src/kata#76-convertBooleanValuesToStrings.jsx
@@ -0,0 +1,9 @@
+/// KATA #76 ///
+
+/* Description:
+
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.*/
+
+function boolToWord( bool ){
+    return bool ? "Yes" : "No";
+  }
