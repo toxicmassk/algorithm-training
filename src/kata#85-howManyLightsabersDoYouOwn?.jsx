332ce03e396377fdb8fb3a23270b5f059c5781ec
Julia Lipniewicz <julia.modex21@gmail.com>
2024-02-14 14:36:21 +0100

    Update kata#85-howManyLightsabersDoYouOwn?.jsx

--- src/kata#85-howManyLightsabersDoYouOwn?.jsx
+++ src/kata#85-howManyLightsabersDoYouOwn?.jsx
@@ -7,7 +7,6 @@ accepts the name of a programmer, and
 returns the number of lightsabers owned by that person.
 The only person who owns lightsabers is Zach, by the way. He owns 18, which is an awesome number of lightsabers. Anyone else owns 0.
 
-Note: your function should have a default parameter.
 
 For example(Input --> Output):
 
