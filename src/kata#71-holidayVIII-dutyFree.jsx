d20609eafe926beabfc00d9cb283a607c1eaf16b
Julia Lipniewicz <julia.modex21@gmail.com>
2024-02-19 23:17:49 +0100

    Update kata#71-holidayVIII-dutyFree.jsx

--- src/kata#71-holidayVIII-dutyFree.jsx
+++ src/kata#71-holidayVIII-dutyFree.jsx
@@ -9,7 +9,7 @@ For example, if a bottle costs £10 normally and the duty free discount is 10%, 
 
 Another example: if a bottle costs £12 normally and the duty free discount is 50%, you would save £6 per bottle. If your holiday will cost £1000, you would have to purchase 166.66 bottles to save £1000, so your answer should be 166 bottles.
 
-All inputs will be integers. Please return an integer. Round down.
+All inputs will be integers. Please return an integer. Round down
 */
 
 function dutyFree(normPrice, discount, hol){
