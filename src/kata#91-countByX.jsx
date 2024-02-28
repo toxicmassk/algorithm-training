2cdec4d29a339974555b4271556857463a303a8a
Julia Lipniewicz <julia.modex21@gmail.com>
2024-02-28 18:10:06 +0100

    Update kata#91-countByX.jsx

--- src/kata#91-countByX.jsx
+++ src/kata#91-countByX.jsx
@@ -13,3 +13,10 @@ countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
 countBy(2,5) === [2,4,6,8,10]
 */
 
+function countBy(x, n) {
+    let z = [];
+    for (let i = 1; i <= n; i++) {
+        z.push(x * i);
+    }
+    return z
+}
