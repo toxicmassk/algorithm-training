f89f6940d98029074a6b58bc78829f9bf4b52325
Julia Lipniewicz <julia.modex21@gmail.com>
2024-02-29 23:00:57 +0100

    Update kata#91-countByX.jsx

--- src/kata#91-countByX.jsx
+++ src/kata#91-countByX.jsx
@@ -6,7 +6,7 @@ Create a function with two arguments that will return an array of the first n mu
 
 Assume both the given number and the number of times to count will be positive numbers greater than 0.
 
-Return the results as an array or list ( depending on language ).
+Return the results as an array or list ( depending on language )
 
 Examples
 countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
