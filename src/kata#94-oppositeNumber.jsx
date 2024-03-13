d2d1075989e494c271fa965460516f56d9c2dfd6
Julia Lipniewicz <julia.modex21@gmail.com>
2024-03-13 13:26:43 +0100

    kata#94-oppositeNumber

---
+++ src/kata#94-oppositeNumber.jsx
@@ -0,0 +1,15 @@
+/// KATA #94 ///
+
+/* Description:
+Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+*/
+
+function opposite(number) {
+    return -number;
+}
