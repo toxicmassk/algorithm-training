33c4cd4561fa59e6ad0185434456df1d5218976f
Julia Lipniewicz <julia.modex21@gmail.com>
2024-03-13 13:40:09 +0100

    Update kata#95-stringCleaning.jsx

--- src/kata#95-stringCleaning.jsx
+++ src/kata#95-stringCleaning.jsx
@@ -10,3 +10,6 @@ Examples (input -> output)
 Your harried co-workers are looking to you for a solution to take this garbled text and remove all of the numbers. Your program will take in a string and clean out all numeric characters, and return a string with spacing and special characters ~#$%^&!@*():;"'.,? all intact.
 */
 
+function stringClean(s) {
+    return s.replace(/\d/g, '');
+}
