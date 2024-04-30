d79d6110605c15cde7af8f279578cfb9ade80b56
Julia Lipniewicz <julia.modex21@gmail.com>
2024-04-30 12:18:27 +0200

    Update kata#102-surfaceAreaAndVolumeOfABox.jsx

--- src/kata#102-surfaceAreaAndVolumeOfABox.jsx
+++ src/kata#102-surfaceAreaAndVolumeOfABox.jsx
@@ -2,4 +2,8 @@
 
 /* Description:
 Write a function that returns the total surface area and volume of a box as an array: [area, volume]
-*/
+*/function getSize(width, height, depth) {
+    let area = 2 * (width * depth) + 2 * (width * height) + 2 * (depth * height);
+    let volume = width * height * depth;
+    return [area, volume];
+} 
