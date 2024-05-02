c88e26347db75387773e13ec0af42436657903fa
Julia Lipniewicz <julia.modex21@gmail.com>
2024-05-02 13:25:17 +0200

    Update kata#103-factorial.jsx

--- src/kata#103-factorial.jsx
+++ src/kata#103-factorial.jsx
@@ -4,3 +4,10 @@
 Your task is to write function factorial.
 */
 
+function factorial(n) {
+    let result = 1;
+    for (let i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
