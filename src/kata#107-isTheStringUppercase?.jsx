3cd2027504ff8722f548f5dd2e19b0fcf02867f4
Julia Lipniewicz <julia.modex21@gmail.com>
2024-05-07 11:14:20 +0200

    Update kata#107-isTheStringUppercase?.jsx

--- src/kata#107-isTheStringUppercase?.jsx
+++ src/kata#107-isTheStringUppercase?.jsx
@@ -17,3 +17,6 @@ Examples (input -> output)
 In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS.
 */
 
+String.prototype.isUpperCase = function () {
+    return this.valueOf().toUpperCase() === this.valueOf();
+}
