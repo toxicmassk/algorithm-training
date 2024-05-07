c03d3a403af815d41e8789f1c40a6073ad8d1c77
Julia Lipniewicz <julia.modex21@gmail.com>
2024-05-07 10:58:20 +0200

    Update kata#106-willYouMakeIt?.jsx

--- src/kata#106-willYouMakeIt?.jsx
+++ src/kata#106-willYouMakeIt?.jsx
@@ -8,3 +8,7 @@ Considering these factors, write a function that tells you if it is possible to 
 
 Function should return true if it is possible and false if not.
 */
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+    return mpg * fuelLeft >= distanceToPump;
+};
