3a8801422443574fbba6b7c79d335196cbaada2b
Julia Lipniewicz <julia.modex21@gmail.com>
2024-05-23 11:36:30 +0200

    Update kata#112-grashopperGradeBook.jsx

--- src/kata#112-grashopperGradeBook.jsx
+++ src/kata#112-grashopperGradeBook.jsx
@@ -12,3 +12,19 @@ Numerical Score	Letter Grade
 0 <= score < 60	'F'
 Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100
 */
+
+
+function getGrade(s1, s2, s3) {
+    var score = (s1 + s2 + s3) / 3;
+    if (score >= 90 && score <= 100) {
+        return 'A';
+    } else if (score >= 80 && score < 90) {
+        return 'B';
+    } else if (score >= 70 && score < 80) {
+        return 'C';
+    } else if (score >= 60 && score < 70) {
+        return 'D';
+    } else {
+        return 'F';
+    }
+};
