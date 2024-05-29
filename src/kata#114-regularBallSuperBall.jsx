4536363479fbb79f62b8baf2e364b44fe3a49827
Julia Lipniewicz <julia.modex21@gmail.com>
2024-05-29 12:36:14 +0200

    Update kata#114-regularBallSuperBall.jsx

--- src/kata#114-regularBallSuperBall.jsx
+++ src/kata#114-regularBallSuperBall.jsx
@@ -3,6 +3,7 @@
 /* Description:
 Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
 
+
 If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
 
 ball1 = new Ball();
