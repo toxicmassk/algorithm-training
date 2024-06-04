01514b574b4d314e96e37b0410978c9c5febb4ec
Julia Lipniewicz <julia.modex21@gmail.com>
2024-06-05 00:45:35 +0200

    Update kata#5-factorBase.jsx

--- src/kata#5-factorBase.jsx
+++ src/kata#5-factorBase.jsx
@@ -15,6 +15,7 @@ You can use the mod operator (%) in most languages to check for a remainder
 For example 2 is not a factor of 7 because: 7 % 2 = 1
 
 Note: base is a non-negative number, factor is a positive number.
+
 */
 
 //Answer:
