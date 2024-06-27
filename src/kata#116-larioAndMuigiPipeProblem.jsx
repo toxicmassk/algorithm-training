812ce25a81feaa01a622e9dd4e4d106d926df335
Julia Lipniewicz <julia.modex21@gmail.com>
2024-06-27 15:20:57 +0200

    Update kata#116-larioAndMuigiPipeProblem.jsx

--- src/kata#116-larioAndMuigiPipeProblem.jsx
+++ src/kata#116-larioAndMuigiPipeProblem.jsx
@@ -12,6 +12,7 @@ The pipes are correct when each pipe after the first is 1 more than the previous
 Task
 Given a list of unique numbers sorted in ascending order, return a new list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included).
 
+
 Example
 Input:  1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,8 */
 
